77403f0d5d3cc285f54f8ec3521a6988deec5bff
Lucas Gabriel <lucasgc2510@gmail.com>
2026-09-01 13:38:34 -0300

    Update contigency.cpp

--- contigency.cpp
+++ contigency.cpp
@@ -2,30 +2,36 @@
 #include <vector>
 
 int main(void){
-    int N, soma;
+    int N, soma, menor_sobra;
     std::cin >> N;
 
-    std::vector<int> armazenados(N);
-    std::vector<int> esperados(N);
-    for (int i = 0; i < N; ++i) {
-        std::cin >> armazenados[i]; 
+    std::vector<std::vector<int>> armazenados(N ,std::vector<int>(2));
+    
+    for (int i = 0; i < N; i++){
+        std::cin >> armazenados[i][0]; 
     }
-    for (int i = 0; i < N; ++i) {
-        std::cin >> esperados[i]; 
+    for (int i = 0; i < N; i++){
+        std::cin >> armazenados[i][1]; 
     }
-    soma = 0;
-    for(int i = N - 1; i >= 0; i--){
-        if(esperados[i] > armazenados[i]){
-            soma = -1;
-            break;
-        }else if(esperados[0] == esperados[i]){
-            soma += esperados[i];
-            break;
-        }else{
-            soma += armazenados[i];
+    menor_sobra = armazenados[0][0] - armazenados[0][1];
+    for(int i = 0; i < N; i++){
+        if(menor_sobra > armazenados[i][0] - armazenados[i][1]){
+            menor_sobra = armazenados[i][0] - armazenados[i][1];
+            elemento[0] = armazenados[i];
         }
-
     }
+
+    if(menor_sobra >= 0){
+        soma = 0;
+        for(int i = 0; i < N; i++){
+            soma += armazenados[i][0];
+        }
+        soma -= menor_sobra;
+
+    }else{
+        soma = -1;
+    }
+   
     std::cout << soma;
 
 
